2a3e7453a00ca4bd9ea53fc312e08b7618cb2042
dbgudrms1 <dbgudrms98@gmail.com>
2022-06-13 04:31:10 +0900

    detail movie5

--- src/main/java/com/Theater/rhg/Service/Service.java
+++ src/main/java/com/Theater/rhg/Service/Service.java
@@ -1,12 +1,12 @@
 package com.Theater.rhg.Service;
 
-import com.Theater.domain.Member;
-import com.Theater.domain.Screen;
-import com.Theater.domain.Ticket;
+import com.Theater.domain.*;
 import com.Theater.rhg.DTO.TicketDTO;
 import com.Theater.rhg.Repository.CustomizedScreenRepositoryImpl;
 import com.Theater.rhg.Repository.CustomizedTicketRepositoryImpl;
 import com.Theater.rhg.Repository.MemberRepo;
+import com.Theater.rhg.Repository.ReviewRepo;
+import com.Theater.ssh.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +18,8 @@ import java.util.List;
 public class Service {
     private final MemberRepo memberRepo;
     private final CustomizedTicketRepositoryImpl customizedTicketRepository;
+    private final ReviewRepo reviewRepo;
+    private final MovieRepository movieRepository;
 
     public Member findMem(String id){
         System.out.println("--------------------service------------------------");
@@ -28,5 +30,17 @@ public class Service {
         System.out.println("--------------------------------------------");
         return customizedTicketRepository.findWithMemberJPQL(id);
 
+    }
+    public void registerReview(Member member,String content,int star,Long movie){
+        Review review=new Review();
+        String loginI=member.getLoginId();
+        Member member1=memberRepo.findOne(loginI);
+        review.setMember(member1);
+        review.setGrade(star);
+        review.setContents(content);
+        review.setMovieLike(0);
+        Movie m=movieRepository.findAllById(movie);
+        review.setMovie(m);
+        reviewRepo.save(review);
     }
 }
